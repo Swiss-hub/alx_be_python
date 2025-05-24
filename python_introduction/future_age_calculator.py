924e7cd6be3760d1aecfbd04d5f61183aab7803e
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-05-25 00:22:24 +0100

    Calc In Py

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,4 @@
+age = int(input("How old are you? "))
+years = int(input("How many years into the future would you like to calculate your age? "))
+future_age = age + years
+print(f"In {years} years, you will be {future_age} years old.")
