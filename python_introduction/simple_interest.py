a46f15c3dc0d63095bf249eaabc4792dcdf503c9
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-05-24 14:17:38 +0100

    Basic Python OPS

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
 principal = 1000
 rate = 0.05
 time = 3
-I = (principal * rate * time)
-print(f"The simple interest is: {I}")
+interest = (principal * rate * time)
+print("The simple interest is:",interest)
 
