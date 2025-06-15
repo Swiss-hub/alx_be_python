5d1698e95a9ae33f58e6260c66f6f2bc78058ad3
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-06-15 21:54:24 +0100

    Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,9 +1,9 @@
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     if operation == "add":
         return num1 + num2
     elif operation == "subtract":
         return num1 - num2
-    elif operation == "multipy":
+    elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
         if num2 == 0:
